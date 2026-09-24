26ebbff895da960e4edbe11f4c18f8be5d8b9ea8
Nishant <nishantmaurya0210@gmail.com>
2026-09-25 00:17:15 +0530

    Added PDF resume text extraction

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const multer=require("multer");
+const {PDFParse}=require("pdf-parse");
+const fs=require("fs");
 
 const app = express();
 
@@ -17,13 +19,35 @@ app.get("/api/health", (req, res) => {
     });
 });
 
-app.post("/api/resume/upload", upload.single("resume"),(req,res)=>{
-    console.log(req.file);
+app.post("/api/resume/upload", upload.single("resume"),async(req,res)=>{
+    try{
+        console.log("File Received: ",req.file.originalname);
 
-    res.json({
-        message:"Resume uploaded Successfully!",
-        file: req.file
-    });
+        const dataBuffer=fs.readFileSync(req.file.path);
+
+        const parser=new PDFParse({
+            data:dataBuffer
+        });
+
+        const result=await parser.getText();
+
+        console.log("Extracted resume text");
+        console.log(result.text);
+
+        await parser.destroy();
+
+        res.json({
+            message:"Resume uploaded and text extracted successfully",
+            text:result.text
+        });
+    }
+    catch(error){
+        console.error("Error processing resume:",error);
+
+        res.status(500).json({
+            message:"Failed to process resume!"
+        });
+    }
 });
 
 app.listen(PORT, () => {
